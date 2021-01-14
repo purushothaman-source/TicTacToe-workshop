7331563506c963e16861d1ca380d7da2dafe3266
purushothaman-source <purushoth99jun@gmail.com>
2021-01-14 19:22:27 +0530

    ADDED : Code to create gameboard

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,7 +1,15 @@
 package com.workshop;
 
 public class Tictactoe {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe game");
+        char[] board=createBoard();
+    }
+    private static char[] createBoard(){
+        char[] board=new char[10];
+        for (int i=0;i< board.length;i++){
+            board[i]=' ';
+        }
+        return board;
     }
 }
