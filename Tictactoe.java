5d2971b4ff76a982fbd11ac5d6137a70cbb7d1bf
purushothaman-source <purushoth99jun@gmail.com>
2021-01-14 19:30:05 +0530

    ADDED : Code to choose user letter

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,9 +1,14 @@
 package com.workshop;
 
+import java.util.Scanner;
+
 public class Tictactoe {
 
     public static void main(String[] args) {
         char[] board=createBoard();
+        Scanner userInput=new Scanner(System.in);
+        char userLetter =chooseUserLetter(userInput);
+        char computeLetter =(userLetter == 'X')?'0':'x';
     }
     private static char[] createBoard(){
         char[] board=new char[10];
@@ -11,5 +16,9 @@ public class Tictactoe {
             board[i]=' ';
         }
         return board;
+    }
+    private static char chooseUserLetter(Scanner userInput) {
+        System.out.println("choose your letter: ");
+        return userInput.next().toUpperCase().charAt(0);
     }
 }
